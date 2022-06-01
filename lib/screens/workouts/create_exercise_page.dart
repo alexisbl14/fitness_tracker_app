963069fcd9380f6ad61798ac279e4b3f11516e63
alexisbl14 <ablydon@ucdavis.edu>
2022-06-01 13:11:09 -0700

    start to create create exercise page

--- lib/screens/workouts/create_exercise_page.dart
+++ lib/screens/workouts/create_exercise_page.dart
@@ -9,15 +9,44 @@ class CreateNewExercisePage extends StatefulWidget {
 }
 
 class _CreateNewExercisePageState extends State<CreateNewExercisePage> {
+  final _exerciseNameController = TextEditingController();
+  final _weightController = TextEditingController();
+  final _setsController = TextEditingController();
+  final _repsController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-          appBar: AppBar(
-            title: Text("New Exercise"),
-            centerTitle: true,
-          ),
-          body: Text("Create New Workout Page")),
+        appBar: AppBar(
+          title: Text("New Exercise"),
+          centerTitle: true,
+          backgroundColor: Colors.lightBlue,
+        ),
+        body: Column(
+          children: [
+            TextField(
+              controller: _exerciseNameController,
+            ),
+            TextField(
+              controller: _weightController,
+            ),
+            Row(
+              children: [
+                TextField(
+                  controller: _setsController,
+                ),
+                Text(" x "),
+                TextField(
+                  controller: _repsController,
+                ),
+              ],
+            ),
+          ],
+        ),
+        floatingActionButton: FloatingActionButton.extended(
+            onPressed: () {}, label: const Text("Create Exercise")),
+      ),
     );
   }
 }
